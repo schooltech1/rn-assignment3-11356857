8708cf9501c9f440d273683e61f192407a1e6175
Yasmin Morrison <yimorrison@st.ug.edu.gh>
2024-05-30 15:36:13 +0000

    updated to include searchbar

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text, ScrollView, StyleSheet, FlatList } from 'react-native';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 
 const App = () => {
   return (
     <ScrollView style={styles.container}>
       <Header />
+      <SearchBar/>
     </ScrollView>
   );
 };
@@ -16,6 +18,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#F9F5EE',
     padding: 20,
   },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  }
 });
 
 export default App;
